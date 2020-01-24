774b971f096c7e5f6965c4491e4771774f40d417
ainpoenya <kurniawan@belidomain.web.id>
2020-01-24 16:10:20 +0700

    rem wifi

--- EchoBot-tes.ino
+++ EchoBot-tes.ino
@@ -1,53 +1,65 @@
-git init/*******************************************************************
- *  An example of bot that echos back any messages received        *
- *  using A101TelegramBot.                                          *
- *                                                                 *
- *  written by Giacarlo Bacchio (Gianbacchio on Github)            *
- *  adapted by Brian Lough                                         *
- *******************************************************************/
 
 
- #include <WiFiSSLClient.h>
- #include <WiFi101.h>
- #include <UniversalTelegramBot.h>
-
-
-// Initialize Wifi connection to the router
-char ssid[] = "xxxx";              // your network SSID (name)
-char password[] = "yyyyyyyyy";                              // your network key
-
-
-
+#include <SPI.h>
+#include <EthernetIndustruino.h>
+#include <UniversalTelegramBot.h>
 // Initialize Telegram BOT
 #define BOTtoken "XXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"  // your Bot Token (Get from Botfather)
-
-WiFiSSLClient client;
 UniversalTelegramBot bot(BOTtoken, client);
 
 int Bot_mtbs = 1000; //mean time between scan messages
 long Bot_lasttime;   //last time messages' scan has been done
 
 
+vbyte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+
+// if you don't want to use DNS (and reduce your sketch size)
+// use the numeric IP instead of the name for the server:
+//IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
+char server[] = "www.google.com";    // name address for Google (using DNS)
+
+// Set the static IP address to use if the DHCP fails to assign
+IPAddress ip(192,168,1,145);
+
+// Initialize the Ethernet client library
+// with the IP address and port of the server 
+// that you want to connect to (port 80 is default for HTTP):
+EthernetClient client;
+
 void setup() {
-
-  Serial.begin(115200);
-
-  delay(100);
-
-  // Attempt to connect to Wifi network:
-  Serial.print("Connecting Wifi: ");
-  Serial.println(ssid);
-  while (WiFi.begin(ssid, password) != WL_CONNECTED) {
-    Serial.print(".");
-    delay(500);
+ // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+   while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
   }
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  IPAddress ip = WiFi.localIP();
-  Serial.println(ip);
 
+  // start the Ethernet connection:
 
+  if (Ethernet.begin(mac) == 0) {
+
+    Serial.println("Failed to configure Ethernet using DHCP");
+    // no point in carrying on, so do nothing forevermore:
+    // try to congifure using IP address instead of DHCP:
+    Ethernet.begin(mac, ip);
+
+  }
+  // give the Ethernet shield a second to initialize:
+  delay(1000);
+  Serial.println("connecting...");
+
+  // if you get a connection, report back via serial:
+  if (client.connect(server, 80)) {
+    Serial.println("connected");
+    // Make a HTTP request:
+    client.println("GET /search?q=arduino HTTP/1.1");
+    client.println("Host: www.google.com");
+    client.println("Connection: close");
+    client.println();
+  } 
+  else {
+    // if you didn't get a connection to the server:
+    Serial.println("connection failed");
+  }
 }
 
 void loop() {
